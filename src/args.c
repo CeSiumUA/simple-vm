431d0ae8402e742a3da704ff3fec93ff49c75534
Fedir Katushonok <fedir.katushonok@gmail.com>
2023-08-03 19:07:54 +0300

    Enhanced argument parsing

--- src/args.c
+++ src/args.c
@@ -1,5 +1,7 @@
 #include "args.h"
 
 void parse_args(int count, char** args, vm_args_t *parsed_args){
-    parsed_args->asm_file_path = args[0];
+    if(count > 1){
+        parsed_args->asm_file_path = args[1];
+    }
 }
--- src/main.c
+++ src/main.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
 int main(int argc, char **argv){
+    vm_args_t vm_args;
+
     printf("Welcome to LC-3 vm!\r\n");
 
+    #ifdef DEBUG_MODE
     printf("Provided arguments:\r\n");
     for(int i = 0; i < argc; i++){
         printf("\t%s\r\n", argv[i]);
     }
+    #endif
+
+    parse_args(argc, argv, &vm_args);
+
+    return 0;
 }
